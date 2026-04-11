f12fb4e1e1b6de09928c6f5869743bd024f1c05f
lewiswatson55 <lewiswatson55@protonmail.com>
2026-04-11 03:41:30 +0100

    Remove backend references for BLIP2 and Florence2

--- frame2kg_train/train/registry.py
+++ frame2kg_train/train/registry.py
@@ -4,8 +4,6 @@ from typing import Dict
 from frame2kg_train.backends.base import VLMBackend
 from frame2kg_train.backends.qwen25_vl import Qwen25VLBackend
 from frame2kg_train.backends.qwen3_vl import Qwen3VLBackend
-from frame2kg_train.backends.blip2 import BLIP2Backend
-from frame2kg_train.backends.florence2 import Florence2Backend
 from frame2kg_train.backends.smolvlm2 import SmolVLM2Backend
 
 
@@ -14,8 +12,6 @@ _REGISTRY: Dict[str, VLMBackend] = {
     "qwen3_vl": Qwen25VLBackend(),
     "qwen35_vl": Qwen25VLBackend(),
     "smolvlm2": SmolVLM2Backend(),
-    "blip2": BLIP2Backend(),
-    "florence2": Florence2Backend(),
 }
 
 
